47d746325cb75737f08c2d495b157bcce567c1a6
xurendong <xrdlyj@163.com>
2018-12-25 15:53:48 +0800

    add basic components

---
+++ main/quantx.bat
@@ -0,0 +1,21 @@
+title QuantX
+@echo off
+cls
+color 0a
+
+echo *******************************************************************************
+echo                           X-Lab (Shanghai) Co., Ltd.
+echo                  Copyright 2018-2019 X-Lab All Rights Reserved.
+echo *******************************************************************************
+
+echo.
+echo Start QuantX
+echo.
+
+D:\Python36\python.exe quantx.pyw
+
+echo.
+echo QuantX Exits
+echo.
+
+pause
---
+++ main/quantx.pyw
@@ -0,0 +1,16 @@
+
+# -*- coding: utf-8 -*-
+
+import sys
+sys.path.append("..")
+sys.path.append("./algo")
+sys.path.append("./face")
+sys.path.append("./port")
+sys.path.append("./tool")
+sys.path.append("../anal")
+sys.path.append("../stra")
+
+import quantx
+
+if __name__ == "__main__":
+    quantx.QuantX()
